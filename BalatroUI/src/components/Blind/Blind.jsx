2c547287d57dcc45cce0b648d72e93096a51c66c
ajiana01 <nur.ajiananta177@gmail.com>
2026-08-19 14:13:19 +0700

    feat: add blind component

---
+++ BalatroUI/src/components/Blind/Blind.jsx
@@ -0,0 +1,34 @@
+﻿import Sprite from '../Sprite/Sprite';
+import { blindSprite } from '../../data/sprites/blindSprites';
+
+function Blind({
+                   blind,
+                   width = 100,
+                   height = 100,
+                   animated = false
+               }) {
+    const blindData = blindSprite.blinds[blind];
+
+    if (!blindData) {
+        console.error(`Blind tidak ditemukan: ${blind}`);
+        return null;
+    }
+
+    return (
+        <Sprite
+            sprite={blindSprite}
+            column={blindData.column}
+            row={blindData.row}
+            width={width}
+            height={height}
+            animated={animated}
+        />
+    );
+}
+
+export default Blind;
+
+//using example
+// import Blind from './components/Blind/Blind';
+//
+// <Blind blind="SmallBlind" />
---
+++ BalatroUI/src/data/sprites/blindSprites.js
@@ -0,0 +1,31 @@
+﻿import image from '../../assets/Balatro-BlindChips.png';
+
+export const blindSprite = {
+    image,
+
+    sheetWidth: 714,
+    sheetHeight: 1054,
+
+    columns: 21,
+    rows: 31,
+
+    cellWidth: 714 / 21,
+    cellHeight: 1054 / 31,
+
+    blinds: {
+        SmallBlind: {
+            column: 0,
+            row: 0
+        },
+
+        BigBlind: {
+            column: 0,
+            row: 1
+        },
+
+        BossBlind: {
+            column: 0,
+            row: 30
+        },
+    }
+};
