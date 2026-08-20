cfcc8bdea55489c7bfecfa748949e711304a00f2
ajiana01 <nur.ajiananta177@gmail.com>
2026-08-20 08:51:11 +0700

    feat: complete boosterpack data

--- BalatroUI/src/data/sprites/boosterPackSprites.js
+++ BalatroUI/src/data/sprites/boosterPackSprites.js
@@ -34,5 +34,62 @@ export const boosterPackSprite = {
         2: { column: 1, row: 1 },
         3: { column: 2, row: 1 },
         4: { column: 3, row: 1 },
+    },
+
+    Celestial_Jumbo: {
+        1: { column: 0, row: 3 },
+        2: { column: 1, row: 3 },
+    },
+
+    Celestial_Mega: {
+        1: { column: 2, row: 3 },
+        2: { column: 3, row: 3 },
+    },
+
+    Standard_Normal: {
+        1: { column: 0, row: 6 },
+        2: { column: 1, row: 6 },
+        3: { column: 2, row: 6 },
+        4: { column: 3, row: 6 },
+    },
+    
+    Standard_Jumbo: {
+        1: { column: 0, row: 7 },
+        2: { column: 1, row: 7 },
+    },
+
+    Standard_Mega: {
+        1: { column: 2, row: 7 },
+        2: { column: 3, row: 7 },
+    },
+
+    Buffon_Normal: {
+        1: { column: 0, row: 8 },
+        2: { column: 1, row: 8 },
+    },
+
+    Buffon_Jumbo: {
+        1: { column: 2, row: 8 },
+    },
+
+    Buffon_Mega: {
+        1: { column: 3, row: 8 },
+    },
+
+    Spectral_Normal: {
+        1: { column: 0, row: 4 },
+        2: { column: 1, row: 4 },
+    },
+
+    Spectral_Jumbo: {
+        1: { column: 2, row: 4 },
+    },
+
+    Spectral_Mega: {
+        1: { column: 3, row: 4 },
+    }
+
+    Blank: {
+        1: { column: 0, row: 5 },
     }
 };
