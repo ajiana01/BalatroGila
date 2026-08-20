22a7fe33690d0c276e505aa01321657d024a6988
ajiana01 <nur.ajiananta177@gmail.com>
2026-08-20 08:43:03 +0700

    feat: complete blind data

--- BalatroUI/src/data/sprites/blindSprites.js
+++ BalatroUI/src/data/sprites/blindSprites.js
@@ -23,7 +23,147 @@ export const blindSprite = {
             row: 1
         },
 
-        BossBlind: {
+        TheOx: {
+            column: 0,
+            row: 2
+        },
+
+        TheHouse: {
+            column: 0,
+            row: 3
+        },
+
+        TheClub: {
+            column: 0,
+            row: 4
+        },
+
+        TheFish: {
+            column: 0,
+            row: 5
+        },
+
+        TheWindow: {
+            column: 0,
+            row: 6
+        },
+
+        TheHook: {
+            column: 0,
+            row: 7
+        },
+
+        TheManacle: {
+            column: 0,
+            row: 8
+        },
+
+        TheWall: {
+            column: 0,
+            row: 9
+        },
+
+        TheWheel: {
+            column: 0,
+            row: 10
+        },
+
+        TheArm: {
+            column: 0,
+            row: 11
+        },
+        
+        ThePsychic: {
+            column: 0,
+            row: 12
+        },
+        
+        TheGoad: {
+            column: 0,
+            row: 13
+        },
+
+        TheWater: {
+            column: 0,
+            row: 14
+        },
+
+        TheSerpent: {
+            column: 0,
+            row: 15
+        },
+
+        ThePillar: {
+            column: 0,
+            row: 16
+        },
+
+        TheEye: {
+            column: 0,
+            row: 17
+        },
+
+        TheMouth: {
+            column: 0,
+            row: 18
+        },
+
+        ThePlant: {
+            column: 0,
+            row: 19
+        },
+
+        TheNeedle: {
+            column: 0,
+            row: 20
+        },
+
+        TheHead: {
+            column: 0,
+            row: 21
+        },
+
+        TheTooth: {
+            column: 0,
+            row: 22
+        },
+
+        TheMark: {
+            column: 0,
+            row: 23
+        },
+
+        TheFlint: {
+            column: 0,
+            row: 24
+        },
+
+        CrimsonHeart: {
+            column: 0,
+            row: 25
+        },
+
+        CeruleanBell: {
+            column: 0,
+            row: 26
+        },
+
+        AmberAcorn: {
+            column: 0,
+            row: 27
+        },
+
+        VerdantLeaf: {
+            column: 0,
+            row: 28
+        },
+
+        VioletVessel: {
+            column: 0,
+            row: 29
+        },
+        
+        Random: {
             column: 0,
             row: 30
         },
