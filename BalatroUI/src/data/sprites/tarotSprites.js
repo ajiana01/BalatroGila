2cf0201dc1f357914d400191cc590e9d9a582231
ajiana01 <nur.ajiananta177@gmail.com>
2026-08-20 09:49:34 +0700

    feat: complete tarot sprite data

--- BalatroUI/src/data/sprites/tarotSprites.js
+++ BalatroUI/src/data/sprites/tarotSprites.js
@@ -29,6 +29,77 @@ export const tarotSprite = {
             column: 3,
             row: 0
         },
-
+        TheEmperor: {
+            column: 4,
+            row: 0
+        },
+        TheHierophant: {
+            column: 5,
+            row: 0
+        },
+        TheLovers: {
+            column: 6,
+            row: 0
+        },
+        TheChariot: {
+            column: 7,
+            row: 0
+        },
+        TheJustice: {
+            column: 8,
+            row: 0
+        },
+        TheHermit: {
+            column: 9,
+            row: 0
+        },
+        TheWheelOfFortune: {
+            column: 0,
+            row: 1
+        },
+        TheStrength: {
+            column: 1,
+            row: 1
+        },
+        TheHangedMan: {
+            column: 2,
+            row: 1
+        },
+        TheDeath: {
+            column: 3,
+            row: 1
+        },
+        TheTemperance: {
+            column: 4,
+            row: 1
+        },
+        TheDevil: {
+            column: 5,
+            row: 1
+        },
+        TheTower: {
+            column: 6,
+            row: 1
+        },
+        TheStar: {
+            column: 7,
+            row: 1
+        },
+        TheMoon: {
+            column: 8,
+            row: 1
+        },
+        TheSun: {
+            column: 9,
+            row: 1
+        },
+        TheJudgement: {
+            column: 0,
+            row: 2
+        },
+        TheWorld: {
+            column: 1,
+            row: 2
+        },
     }
 };
